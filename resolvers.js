0ab1dd3b992daa8e0e283e33efd2b7c3af55c30e
= <=>
2020-02-23 18:32:13 +0900

    google login

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require("apollo-server");
+
 const user = {
   _id: "1",
   name: "Yoshiji",
@@ -5,8 +7,15 @@ const user = {
   picture: "https://via.placeholder.com/150/0000FF/808080?Text=Digital.com"
 };
 
+const authenticated = next => (root, args, ctx, info) => {
+  if (!ctx.currentUser) {
+    throw new AuthenticationError("You must be logged in");
+  }
+  return next(root, args, ctx, info);
+};
+
 module.exports = {
   Query: {
-    me: () => user
+    me: authenticated((root, args, ctx) => ctx.currentUser)
   }
 };
